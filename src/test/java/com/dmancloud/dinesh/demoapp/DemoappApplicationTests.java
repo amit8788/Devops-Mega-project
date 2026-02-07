6667f7a8b75f766a67aa00342e79f77760777aac
Devops1224789 <devopsstudy09@gmail.com>
2026-02-07 20:56:34 +0530

    error maven

--- src/test/java/com/dmancloud/dinesh/demoapp/DemoappApplicationTests.java
+++ src/test/java/com/dmancloud/dinesh/demoapp/DemoappApplicationTests.java
@@ -3,11 +3,14 @@ package com.dmancloud.dinesh.demoapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class DemoappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Intentional failure for pipeline testing
+        assertEquals(1, 2);
+    }
 }
